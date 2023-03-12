6dd090f14b97defa9accdeb772d196835b537675
Kristian Atanasov <124519664+atanasov23@users.noreply.github.com>
2023-03-12 22:54:22 +0200

    adding a user dropdown menu

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -7,7 +7,16 @@ export function Navigation() {
         <nav className="navigation">
             <Link to="/user/messages">Съобщения</Link>
             <Link to="/user/fav">Любими</Link>
-            <Link to="/user/profile">Профил</Link>
+            <Link to="/user/profile">
+                <div class="dropdown">
+                    <button class="dropbtn">Профил</button>
+                    <div class="dropdown-content">
+                        <Link to="#">Link 1</Link>
+                        <Link to="#">Link 2</Link>
+                        <Link to="#">Link 3</Link>
+                    </div>
+                </div>
+            </Link>
         </nav>
 
     )
